4261d951ebf4f5f171b09d501b9091fb3395e787
Sweyd Manaf <abdulsweyd@gmail.com>
2023-02-03 16:56:54 +0200

    Atualizando atributos HTML com js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function toggleMode(){
   const html = document.documentElement
+  html.classList.toggle("light")
 
-  // if(html.classList.contains('light')) {
-  //   html.classList.remove('light')
-  // } else {
-  //   html.classList.add('light')
-  // }
+  // pegar a tag img
+  const img = document.querySelector('#profile img')
 
-  html.classList.toggle('light')
-
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/Avatar-light.png')
+  } else {
+    // se tiver sem light mode, adicionar a imagem normal
+    img.setAttribute('src', './assets/Avatar.png')
+  } 
 }
